b418ccf78e50a19aa3cf4e26e9f682167e648826
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-10-21 19:18:29 +0000

    Removed unused imports

--- src/main/java/me/ferrybig/minecraftcron/Main.java
+++ src/main/java/me/ferrybig/minecraftcron/Main.java
@@ -12,17 +12,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
